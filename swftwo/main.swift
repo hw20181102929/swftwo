285b368346f2f35e400ea4a019bf24e1d99bc266
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-20 08:42:11 +0800

    Initial Commit

---
+++ swftwo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swftwo
+//
+//  Created by s20181102929 on 2019/9/20.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
