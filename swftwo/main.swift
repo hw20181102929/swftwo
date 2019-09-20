af2a41ec324722091545429ff54ff07a28256a61
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-09-20 10:03:17 +0800

    sw
    sw

--- swftwo/main.swift
+++ swftwo/main.swift
@@ -1,12 +1,48 @@
-//
-//  main.swift
-//  swftwo
-//
-//  Created by s20181102929 on 2019/9/20.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
+var array : [Int] = [3,5,8,7,9,10,22,101]
+for (index,item) in array.enumerated()
+{
+    print(array[(index+1)%array.count]);
+}
+var t = 0
+for(index,item) in array.enumerated()
+{
+    if index < array.count - 1
+    {
+    t = array[index]
+    array[index] = array[index+1]
+    array[index+1] = t
+    }
+}
+for index in array
+{
+    print(index)
+}
+func find(a:Int,b:Int) -> Int
+{
+    if a>b{
+    return a
+    }
+    else{
+    return b
+    }
+}
+print(find(a:10,b:11))
+func sum(array:[Int])->Int
+{
+    var sum = 0
+    for index in array
+    {
+        sum = sum + index
+    }
+    return sum;
+}
+var array1 = [Int] ()
+var input = readLine()!
+while(input != "end")
+{
+    var change = Int(input)!
+    array1 += [change]
+    input = readLine()!
+}
+print("sum=\(sum(array:array1))")
 
